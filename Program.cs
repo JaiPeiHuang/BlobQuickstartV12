e78cb04f464b8f39975dba6d59295e1b8775d37b
JaiPeiHunag <610090238@qq.com>
2021-02-04 11:58:25 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobQuickstartV12
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            //获取连接字符串
+            Console.WriteLine("Azure Blob Storage v12 - .NET quickstart sample\n");
+
+            // Retrieve the connection string for use with the application. The storage
+            // connection string is stored in an environment variable on the machine
+            // running the application called AZURE_STORAGE_CONNECTION_STRING. If the
+            // environment variable is created after the application is launched in a
+            // console or with Visual Studio, the shell or application needs to be closed
+            // and reloaded to take the environment variable into account.
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+
+            //创建一个容器
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            string containerName = "container-example";
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient =  blobServiceClient.GetBlobContainerClient(containerName);
+
+
+            //将Blob上传到容器
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "../../../data/";
+            string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+            //System.IO.File.Create(localFilePath);
+
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.get(fileName);
+
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            // Open the file and upload its data
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+
+
+            //列出容器中的斑点
+            Console.WriteLine("Listing blobs...");
+
+            // List all blobs in the container
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine("\t" + blobItem.Name);
+            }
+
+
+            //下载Blob
+
+
+            // Download the blob to a local file
+            // Append the string "DOWNLOADED" before the .txt extension 
+            // so you can compare the files in the data directory
+            string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOADED.txt");
+
+            Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+            // Download the blob's contents and save it to a file
+            BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+            using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+            {
+                await download.Content.CopyToAsync(downloadFileStream);
+                downloadFileStream.Close();
+            }
+
+
+            //删除容器
+            // Clean up
+            Console.Write("Press any key to begin clean up");
+            Console.ReadLine();
+
+            Console.WriteLine("Deleting blob container...");
+            await containerClient.DeleteAsync();
+
+            Console.WriteLine("Deleting the local source and downloaded files...");
+            File.Delete(localFilePath);
+            File.Delete(downloadFilePath);
+
+            Console.WriteLine("Done");
+
+
+        }
+    }
+}
